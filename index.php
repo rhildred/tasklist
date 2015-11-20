43e04332c6236032f9140cee3916c3c65901401a
rhildred <rhildred@gmail.com>
2015-11-20 09:08:06 -0500

    got front end communicating with back end

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 require "vendor\autoload.php";
 
 $oApp = new \Slim\Slim(array('templates.path' => __DIR__));
+// open databaase
+$oDb = new PDO("sqlite:" . __DIR__ . "/amounts.sqlite");
 
 $oApp->get("/", function() use($oApp){
     $oApp->render("addingMachine.phtml");
+});
+
+$oApp->post("/", function() use($oApp, $oDb){
+    $oData = json_decode($oApp->request->getBody());
+    print_r($oData);
 });
 
 $oApp->run();
