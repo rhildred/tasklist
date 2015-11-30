50bd01d485c028aec890dc991eeb2f6e2d89a931
rhildred <rhildred@gmail.com>
2015-11-30 08:05:36 -0500

    updated amounts to be tasklist

--- index.php
+++ index.php
@@ -4,15 +4,18 @@ require "vendor\autoload.php";
 
 $oApp = new \Slim\Slim(array('templates.path' => __DIR__));
 // open databaase
-$oDb = new PDO("sqlite:" . __DIR__ . "/amounts.sqlite");
+$oDb = new PDO("sqlite:" . __DIR__ . "/tasks.sqlite");
 
 $oApp->get("/", function() use($oApp){
-    $oApp->render("addingMachine.phtml");
+    $oApp->render("tasks.phtml");
 });
 
-$oApp->post("/", function() use($oApp, $oDb){
+$oApp->post("/tasks", function() use($oApp, $oDb){
     $oData = json_decode($oApp->request->getBody());
-    print_r($oData);
+    $oStmt = $oDb->prepare("INSERT INTO tasks(description) VALUES(:task)");
+    $oStmt->bindParam("task", $oData->currentTask);
+    $oStmt->execute();
+    echo json_encode(array("rows"=>$oStmt->rowCount()));
 });
 
 $oApp->run();
