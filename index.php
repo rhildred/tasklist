8349f92a7fd23e425f7af23f03e010be780d37e3
rhildred <rhildred@gmail.com>
2015-11-30 09:57:53 -0500

    got complete crud going

--- index.php
+++ index.php
@@ -10,12 +10,35 @@ $oApp->get("/", function() use($oApp){
     $oApp->render("tasks.phtml");
 });
 
+$oApp->get("/tasks", function() use($oApp, $oDb){
+    $oStmt = $oDb->prepare("SELECT * FROM tasks");
+    $oStmt->execute();
+    $aTasks = $oStmt->fetchAll(PDO::FETCH_OBJ);
+    echo json_encode($aTasks);
+});
+
 $oApp->post("/tasks", function() use($oApp, $oDb){
     $oData = json_decode($oApp->request->getBody());
     $oStmt = $oDb->prepare("INSERT INTO tasks(description) VALUES(:task)");
     $oStmt->bindParam("task", $oData->currentTask);
     $oStmt->execute();
     echo json_encode(array("rows"=>$oStmt->rowCount()));
+});
+
+$oApp->post("/tasks/:id", function($id) use($oApp, $oDb){
+    $oData = json_decode($oApp->request->getBody());
+    $oStmt = $oDb->prepare("UPDATE tasks SET finished = 1 WHERE id = :id");
+    $oStmt->bindParam("id", $id);
+    $oStmt->execute();
+    echo json_encode(array("rows"=>$oStmt->rowCount()));
+});
+
+$oApp->delete("/tasks/:id", function($id) use($oApp, $oDb){
+    $oStmt = $oDb->prepare("DELETE FROM tasks WHERE id = :id");
+    $oStmt->bindParam("id", $id);
+    $oStmt->execute();
+    echo json_encode(array("rows"=>$oStmt->rowCount()));
+
 });
 
 $oApp->run();
