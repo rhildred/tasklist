771a3e442b82707cd6db6df85b063ecc24e7beaf
Rich Hildred <rhildred@gmail.com>
2015-11-30 22:25:36 -0500

    protected /tasks resource

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require "vendor\autoload.php";
+require "vendor/autoload.php";
 
 $oApp = new \Slim\Slim(array('templates.path' => __DIR__));
 // open databaase
@@ -10,14 +10,14 @@ $oApp->get("/", function() use($oApp){
     $oApp->render("tasks.phtml");
 });
 
-$oApp->get("/tasks", function() use($oApp, $oDb){
+$oApp->get("/tasks", new \Auth(), function() use($oApp, $oDb){
     $oStmt = $oDb->prepare("SELECT * FROM tasks");
     $oStmt->execute();
     $aTasks = $oStmt->fetchAll(PDO::FETCH_OBJ);
     echo json_encode($aTasks);
 });
 
-$oApp->post("/tasks", function() use($oApp, $oDb){
+$oApp->post("/tasks", new \Auth(), function() use($oApp, $oDb){
     $oData = json_decode($oApp->request->getBody());
     $oStmt = $oDb->prepare("INSERT INTO tasks(description) VALUES(:task)");
     $oStmt->bindParam("task", $oData->currentTask);
@@ -25,7 +25,7 @@ $oApp->post("/tasks", function() use($oApp, $oDb){
     echo json_encode(array("rows"=>$oStmt->rowCount()));
 });
 
-$oApp->post("/tasks/:id", function($id) use($oApp, $oDb){
+$oApp->post("/tasks/:id", new \Auth(), function($id) use($oApp, $oDb){
     $oData = json_decode($oApp->request->getBody());
     $oStmt = $oDb->prepare("UPDATE tasks SET finished = 1 WHERE id = :id");
     $oStmt->bindParam("id", $id);
@@ -33,12 +33,41 @@ $oApp->post("/tasks/:id", function($id) use($oApp, $oDb){
     echo json_encode(array("rows"=>$oStmt->rowCount()));
 });
 
-$oApp->delete("/tasks/:id", function($id) use($oApp, $oDb){
+$oApp->delete("/tasks/:id", new \Auth(), function($id) use($oApp, $oDb){
     $oStmt = $oDb->prepare("DELETE FROM tasks WHERE id = :id");
     $oStmt->bindParam("id", $id);
     $oStmt->execute();
     echo json_encode(array("rows"=>$oStmt->rowCount()));
 
+});
+
+$oApp->get("/login", function() use( $oApp){
+    // see if this is the original redirect or if it's the callback
+    $sCode = $oApp->request->params('code');
+    // get the uri to redirect to
+    $sUrl = "http";
+    if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443)
+    {
+        $sUrl .= "s";
+    }
+    $sUrl .= "://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+
+    $oAuth = new \Oauth2($sUrl);
+    if($sCode == null){
+        $oApp->response->redirect($oAuth->redirectUrl());
+    }else{
+        $oAuth->handleCode($sCode);
+        $oApp->response->redirect("/");
+    }
+});
+
+$oApp->get("/currentUser", function() use($oApp){
+    echo json_encode($_SESSION['CurrentUser']);
+});
+
+$oApp->get("/logout", function(){
+    session_start();
+    unset($_SESSION["CurrentUser"]);
 });
 
 $oApp->run();
